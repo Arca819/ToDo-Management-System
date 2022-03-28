24dbf7eecd76b8f640bfb432970fd3cd76022772
Arca819 <101073133+Arca819@users.noreply.github.com>
2022-03-29 00:01:32 +0900

    コントローラー作成完了

--- src/main/java/com/dmm/task/controller/TaskController.java
+++ src/main/java/com/dmm/task/controller/TaskController.java
@@ -1,29 +1,123 @@
 package com.dmm.task.controller;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.dmm.task.data.entity.Tasks;
+import com.dmm.task.form.TaskForm;
+import com.dmm.task.service.AccountUserDetails;
+import com.dmm.task.service.TasksService;
 
 @Controller
 public class TaskController {
+	@Autowired
+	private TasksService tasksService;
+
 	@GetMapping("/main")
-	public String main() {
+	public String list(@AuthenticationPrincipal AccountUserDetails user, Model model) {
+		// 今月の月名を設定
+		LocalDate myDate = LocalDate.now();
+		int myYear = myDate.getYear();
+		int myMonth = myDate.getMonthValue();
+		model.addAttribute("month", myYear + "年" + myMonth + "月");
+
+		// 今月1日の曜日
+		LocalDate m1 = LocalDate.of(myYear, myMonth, 1);
+		int wNo = m1.getDayOfWeek().getValue();
+		// 今月1日を含む週の初日（日曜日）
+		LocalDate firstDate = m1;
+		if (wNo != 7) {
+			firstDate = m1.minusDays(wNo);
+		}
+		// 今月末日の曜日
+		int de = m1.lengthOfMonth();
+		LocalDate me = LocalDate.of(myYear, myMonth, de);
+		int we = me.getDayOfWeek().getValue();
+		// 今月末日を含む週の末日（土曜日）
+		LocalDate lastDate = me.plusDays(6 - (we % 7));
+
+		// カレンダーマトリクスの作成
+		List<List<LocalDate>> matrix = new ArrayList<>();
+		LocalDate d1 = firstDate;
+		while (d1.compareTo(lastDate) <= 0) {
+			List<LocalDate> weekDays = new ArrayList<>();
+			for (int i = 0; i < 7; i++) {
+				weekDays.add(d1);
+				d1 = d1.plusDays(1);
+			}
+			matrix.add(weekDays);
+		}
+		model.addAttribute("matrix", matrix);
+
+		Map<LocalDate, List<Tasks>> tasks = new HashMap<>();
+		LocalDate day = firstDate;
+		while (day.compareTo(lastDate) <= 0) {
+			List<Tasks> taskList = tasksService.selectByDateBetweenName(day, day, user.getName());
+			tasks.put(day, taskList);
+			day = day.plusDays(1);
+		}
+		model.addAttribute("tasks", tasks);
+
 		return "main";
 	}
 
-	@GetMapping("/main/create/{date}")
-	public String create(@PathVariable("date") String date, Model model) {
+	@GetMapping("/create/{date}")
+	public String post(@PathVariable("date") String date, @ModelAttribute TaskForm taskForm, Model model) {
 		LocalDate myDate = LocalDate.parse(date);
 		model.addAttribute("date", myDate);
 		return "create";
 	}
 
-	@GetMapping("/main/edit/{id}")
-	public String edit(@PathVariable("id") Integer id) {
+	@PostMapping("/create")
+	public String regist(@ModelAttribute TaskForm taskForm, @AuthenticationPrincipal AccountUserDetails user) {
+		Tasks task = new Tasks();
+		task.setTitle(taskForm.getTitle());
+		task.setName(user.getName());
+		task.setText(taskForm.getText());
+		task.setDate(taskForm.getDate());
+		task.setDone(false);
+		tasksService.insert(task);
+		return "redirect:/main";
+	}
+
+	@GetMapping("/edit/{id}")
+	public String edit(@PathVariable("id") Integer id, @ModelAttribute Tasks task, Model model) {
+		Optional<Tasks> taskOptional = tasksService.selectById(id);
+		task = taskOptional.get();
+		model.addAttribute("task", task);
 		return "edit";
 	}
 
+	@PostMapping("edit/{id}")
+	public String regist(@PathVariable("id") Integer id, @ModelAttribute TaskForm taskForm,
+			@AuthenticationPrincipal AccountUserDetails user) {
+		Tasks task = new Tasks();
+		task.setId(id);
+		task.setTitle(taskForm.getTitle());
+		task.setName(user.getName());
+		task.setText(taskForm.getText());
+		task.setDate(taskForm.getDate());
+		task.setDone(taskForm.getDone());
+		tasksService.update(task);
+		return "redirect:/main";
+	}
+
+	@PostMapping("delete/{id}")
+	public String delete(@PathVariable("id") Integer id) {
+		tasksService.delete(id);
+		return "redirect:/main";
+	}
 }
